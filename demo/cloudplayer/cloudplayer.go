61574ed79b12ef3a4e9adf205763b2b1dc010314
lipeijian17 <90599687+lipeijian17@users.noreply.github.com>
2023-04-11 10:42:26 +0800

    Update cloudplayer.go

--- demo/cloudplayer/cloudplayer.go
+++ demo/cloudplayer/cloudplayer.go
@@ -13,12 +13,12 @@ import (
 	"time"
 )
 
-var appID = "9a3b95751df14cebaf8f155448ee0e36"
-var appCertificate = "9654e7de17154329a55e716830e07d19"
+var appID = "9a3bXXXXXXXXXXXXXXXXXXXXXe36"
+var appCertificate = "9654XXXXXXXXXXXXXXXXXXXXX07d19"
 
 // 客户 ID  客户密钥
-var customerKey = "33ebb114b3284064bf7d049e16f4d696"
-var customerSecret = "c80110826fc74e45a9559518176a2f9a"
+var customerKey = "33eXXXXXXXXXXXXXXXXXXXXX96"
+var customerSecret = "c80XXXXXXXXXXXXXXXXXXXXX2f9a"
 var timestamp = time.Now().UTC().String()
 
 // 拼接客户 ID 和客户密钥并使用 base64 进行编码
@@ -67,7 +67,6 @@ func CreatCloudPlayer(token string) (playerid string, err error) {
     }`, "Token", token, -1))
 
 	client := &http.Client{}
-	//作为client端生成的request body，需不需要手动关闭呢，答案是不需要的
 	req, err := http.NewRequest(method, url, payload)
 
 	if err != nil {
@@ -89,8 +88,6 @@ func CreatCloudPlayer(token string) (playerid string, err error) {
 
 	//打印response
 	fmt.Println(res)
-
-	//作为client端处理response的时候，有一点要注意的是，body一定要close，否则会造成GC回收不到，继而产生内存泄露；
 	defer res.Body.Close()
 
 	//这里需要修改
@@ -220,7 +217,6 @@ func ListCloudPlayer() (string, error) {
 	return playercount.(string), nil
 }
 
-//对Http请求的返回结果转map处理
 func ParseResponse(response *http.Response) map[string]interface{} {
 	var result map[string]interface{}
 	body, err := ioutil.ReadAll(response.Body)
@@ -254,6 +250,7 @@ func WriteFile_playerid(playerid string) {
 
 //定义读取文件
 func ReadFile_playerid() (result string) {
+        //此处的路径记得修改为自己环境的相对路径或者绝对路径，否则会报错
 	file, err := os.Open("/Users/lpj/go/src/demo/cloudplayer/playerid_info.json") //打开
 	if err != nil {
 		fmt.Println(err)
